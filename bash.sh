d3dc31684280369422ac436a7c9dd9aeb5d8f817
Sandhu-Sahil <sandhu.sahil2002@gmail.com>
2024-08-16 13:30:14 +0530

    mapp roaming built

--- bash.sh
+++ bash.sh
@@ -1,9 +1,16 @@
 # setup adb wireless
 
 adb kill-server
+sleep 5
 adb start-server
+sleep 5
 adb devices
+sleep 5
 
 adb usb
+sleep 5
 adb tcpip 5555
+sleep 5
 adb connect 192.168.29.177:5555
+sleep 5
+adb devices
